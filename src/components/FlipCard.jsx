0835d2e4c16510ec6b89d939e5c92c4ee85b4486
Krystle Murphy <120310009+KrystleMurphy@users.noreply.github.com>
2024-03-14 19:05:06 +0000

    changing class to className in flipcard comp.

--- src/components/FlipCard.jsx
+++ src/components/FlipCard.jsx
@@ -5,40 +5,40 @@ function FlipCard ()
 {
     return (
         <>
-        <div class="flip-card">
-    <div class="flip-card-inner">
-        <div class="flip-card-front">
-            <p class="title">FLIP CARD</p>
+        <div className="flip-card">
+    <div className="flip-card-inner">
+        <div className="flip-card-front">
+            <p className="title">FLIP CARD</p>
             <p>Hover Me</p>
         </div>
-        <div class="flip-card-back">
-            <p class="title">BACK</p>
+        <div className="flip-card-back">
+            <p className="title">BACK</p>
             <p>Leave Me</p>
         </div>
     </div>
 </div>
 
-<div class="flip-card">
-    <div class="flip-card-inner">
-        <div class="flip-card-front">
-            <p class="title">FLIP CARD</p>
+<div className="flip-card">
+    <div className="flip-card-inner">
+        <div className="flip-card-front">
+            <p className="title">FLIP CARD</p>
             <p>Hover Me</p>
         </div>
-        <div class="flip-card-back">
-            <p class="title">BACK</p>
+        <div className="flip-card-back">
+            <p className="title">BACK</p>
             <p>Leave Me</p>
         </div>
     </div>
 </div>
 
-<div class="flip-card">
-    <div class="flip-card-inner">
-        <div class="flip-card-front">
-            <p class="title">FLIP CARD</p>
+<div className="flip-card">
+    <div className="flip-card-inner">
+        <div className="flip-card-front">
+            <p className="title">FLIP CARD</p>
             <p>Hover Me</p>
         </div>
-        <div class="flip-card-back">
-            <p class="title">BACK</p>
+        <div className="flip-card-back">
+            <p className="title">BACK</p>
             <p>Leave Me</p>
         </div>
     </div>
