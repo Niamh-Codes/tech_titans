8f0cc6e4de92a1103a99357cc7bd98e1755e835f
Krystle Murphy <120310009+KrystleMurphy@users.noreply.github.com>
2024-03-14 19:29:13 +0000

    updating the flip cards with correct div closing tags

--- src/components/FlipCard.jsx
+++ src/components/FlipCard.jsx
@@ -5,6 +5,7 @@ function FlipCard ()
 {
     return (
         <>
+        <div id="flip-card-1">
         <div className="flip-card">
     <div className="flip-card-inner">
         <div className="flip-card-front">
@@ -17,7 +18,9 @@ function FlipCard ()
         </div>
     </div>
 </div>
+</div>
 
+<div id="flip-card-2">
 <div className="flip-card">
     <div className="flip-card-inner">
         <div className="flip-card-front">
@@ -30,8 +33,10 @@ function FlipCard ()
         </div>
     </div>
 </div>
+</div>
 
-<div className="flip-card">
+<div id="flip-card-3">
+    <div className="flip-card">
     <div className="flip-card-inner">
         <div className="flip-card-front">
             <p className="title">FLIP CARD</p>
@@ -41,6 +46,7 @@ function FlipCard ()
             <p className="title">BACK</p>
             <p>Leave Me</p>
         </div>
+    </div>
     </div>
 </div>
         </>
