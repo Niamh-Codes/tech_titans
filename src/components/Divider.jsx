8bbba96df17231ebf830ac6481e83f0d21962be3
Nkosinathi Prince Hiyashe <nphiyashe@gmail.com>
2024-03-15 23:03:20 +0000

    Changed class to clasName in Divider.jsx

--- src/components/Divider.jsx
+++ src/components/Divider.jsx
@@ -4,7 +4,7 @@ import "./Divider.css";
 function Divider () {
     return (
         <hr style={{ backgroundColor: '#1B1914', color: '#FFB53A' }}
-        class="my-12 h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-neutral-500 to-transparent dark:via-neutral-400" />
+        className="my-12 h-px border-t-0 bg-transparent bg-gradient-to-r from-transparent via-neutral-500 to-transparent dark:via-neutral-400" />
     );
 }
 
