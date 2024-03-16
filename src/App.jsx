eee10ddf593e742657b073bc83a6d8b461258b07
avocado.codes <151567906+Niamh-Codes@users.noreply.github.com>
2024-03-16 16:20:48 +0000

    fixing navbar issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,6 @@
 // insert the import modules here
 import Navbar from "./components/Navbar";
 import Header from "./components/Header";
-<<<<<<< HEAD
-=======
-import Navbar from "./components/navbar";
->>>>>>> cd8d94e770c7c77cd9a3efa903d0d6b886409331
 import MainSearch from "./components/MainSearch";
 import Footer from "./components/Footer";
 import Quiz from "./components/Quiz";
