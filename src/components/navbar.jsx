eaebe22c66f47f2d475f56fdc82f3d30ed721bd9
Krystle Murphy <120310009+KrystleMurphy@users.noreply.github.com>
2024-03-16 21:07:49 +0000

    removing shadow css codes that appear mid section

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -3,17 +3,15 @@ import React from "react";
 function Navbar() {
   return (
     <>
-      <nav className="bg-gray-900 text-white py-2 lg:py-4 shadow-md dark:bg-neutral-700">
+      <nav style={{ backgroundColor: '#1B1914', color: '#FFB53A' }} className=" py-2 lg:py-4 dark:bg-neutral-700">
         <div className="container mx-auto px-3 lg:px-0 flex items-center justify-between">
-          <span className="text-xl lg:text-2xl">*</span>
+          <span className="text-xl lg:text-2xl ml-8">*</span>
           <div className="flex w-full lg:w-1/3 items-center justify-between">
             <span className="flex items-center px-3 py-2 text-gray-600 dark:text-white"></span>
           </div>
-          <div className="hidden lg:flex w-full lg:w-1/3 items-center justify-between">
-            <ul className="flex gap-6">
-              <li>About</li>
-              <li>Contact</li>
-              <li>Help</li>
+          <div className="hidden lg:flex w-full lg:w-1/3 items-center justify-end mr-8">
+            <ul className="flex gap-4">
+              <a href="#footer">Contact</a>
             </ul>
           </div>
         </div>
