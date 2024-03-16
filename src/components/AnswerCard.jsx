8b209307b0648807a25ccbb0d7b51475269fedd7
Krystle Murphy <120310009+KrystleMurphy@users.noreply.github.com>
2024-03-16 19:54:46 +0000

    trying to dynamically generate the fact cards from API data

--- src/components/AnswerCard.jsx
+++ src/components/AnswerCard.jsx
@@ -1,23 +1,44 @@
 import React from "react";
 import "./AnswerCard.css";
 
-function AnswerCard () 
-{
-    return (
-        <>
 
-        <div className="flip-card">
-    <div className="flip-card-inner">
-        <div className="flip-card-front">
-            <p id="data">Hover Me</p>
-        </div>
-        <div className="flip-card-back">
-            <p id="information">Leave Me</p>
-        </div>
-    </div>
-</div>
-        </>
-    )
-};
+    function AnswerCard({ countryCapital, countryPopulation, countryArea }) {
+        return (
+            <>
+                <div className="flip-card">
+                    <div className="flip-card-inner">
+                        <div className="flip-card-front">
+                            <p id="data">Capital</p>
+                        </div>
+                        <div className="flip-card-back">
+                            <p id="information">{countryCapital}</p>
+                        </div>
+                    </div>
+                </div>
+    
+                <div className="flip-card">
+                    <div className="flip-card-inner">
+                        <div className="flip-card-front">
+                            <p id="data">Population</p>
+                        </div>
+                        <div className="flip-card-back">
+                            <p id="information">{countryPopulation}</p>
+                        </div>
+                    </div>
+                </div>
+    
+                <div className="flip-card">
+                    <div className="flip-card-inner">
+                        <div className="flip-card-front">
+                            <p id="data">Area</p>
+                        </div>
+                        <div className="flip-card-back">
+                            <p id="information">{countryArea}</p>
+                        </div>
+                    </div>
+                </div>
+            </>
+        );
+    }
 
 export default AnswerCard;
