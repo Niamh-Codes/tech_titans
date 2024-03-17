52ca9c8fadac03e420bed23cf91147e49567cbad
Krystle Murphy <120310009+KrystleMurphy@users.noreply.github.com>
2024-03-17 10:28:40 +0000

    changing the quiz button styles

--- src/components/QuizCards.jsx
+++ src/components/QuizCards.jsx
@@ -5,7 +5,7 @@ function QuizCards() {
         <>
       <button
         type="button" 
-        className="inline-block rounded bg-primary px-7 pb-2.5 pt-3 text-sm font-medium uppercase leading-normal text-white shadow-primary-3 transition duration-150 ease-in-out hover:bg-primary-accent-300 hover:shadow-primary-2 focus:bg-primary-accent-300 focus:shadow-primary-2 focus:outline-none focus:ring-0 active:bg-primary-600 active:shadow-primary-2 motion-reduce:transition-none dark:shadow-black/30 dark:hover:shadow-dark-strong dark:focus:shadow-dark-strong dark:active:shadow-dark-strong">
+        className="inline-block mx-auto bg-customOrange justify-center rounded text-sm font-medium uppercase leading-normal text-black">
         AnswerOptions
       </button>
 </>
