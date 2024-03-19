5d60b1209d87ccac54283b9ab8aa95223c0310c4
Krystle Murphy <120310009+KrystleMurphy@users.noreply.github.com>
2024-03-19 20:27:30 +0000

    adding logo to top left nav bar

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,23 +1,24 @@
 import React from "react";
+import helloCuriosvg from '../../assets/helloCuriosvg.svg'
 
 
 
 function Navbar() {
   return (
     <>
-      <nav className="text-customOrange bg-body py-2 lg:py-4 shadow-md dark:bg-neutral-700">
-        <div className="container mx-auto px-3 lg:px-0 flex items-center justify-between">
-          <span className="text-xl lg:text-2xl ml-8">HelloCurio - import starOrange here! </span>
-          <div className="flex w-full lg:w-1/3 items-center justify-between">
-            <span className="flex items-center px-3 py-2 text-gray-600 dark:text-white"></span>
-          </div>
-          <div className="hidden lg:flex w-full lg:w-1/3 items-center justify-end mr-8">
-            <ul className="flex gap-4">
-              <a href="#footer">Contact</a>
-            </ul>
-          </div>
-        </div>
-      </nav>
+<nav className="text-customOrange bg-body py-2 lg:py-4 shadow-md dark:bg-neutral-700">
+  <div className="container mx-auto px-3 lg:px-0 flex items-center justify-between">
+    <img src='../../assets/helloCuriosvg.svg' alt='orange star logo' className="h-8 w-8 lg:h-10 lg:w-10 ml-8"/>
+    <div className="flex w-full lg:w-1/3 items-center justify-between">
+      <span className="flex items-center px-3 py-2 text-gray-600 dark:text-white"></span>
+    </div>
+    <div className="hidden lg:flex w-full lg:w-1/3 items-center justify-end mr-8">
+      <ul className="flex gap-4">
+        <a href="#footer">Contact</a>
+      </ul>
+    </div>
+  </div>
+</nav>
     </>
   );
 }
