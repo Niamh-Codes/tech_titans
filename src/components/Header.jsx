a0c80789e7e2e5c2c9bdd80a80157c9b25bc2350
avocado.codes <151567906+Niamh-Codes@users.noreply.github.com>
2024-03-19 21:20:42 +0000

    adding svg to header and changing bg

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,36 @@
 import React from 'react';
 import { motion } from 'framer-motion';
+import starOrange from '../../assets/starOrange.svg';
 
 function Header() {
   return (
-    <div className=" 100-vh mx p-6 bg-body flex flex-col flexlg:flex-row px-6">
+    <div className=" 100-vh mx p-6  flex flex-col flexlg:flex-row px-6">
       <div>
         <motion.h1
           initial={{ opacity: 0, y: -200 }}
           animate={{ opacity: 1, y: -100 }}
           transition={{ duration: 1 }}
-          className="mb-2  100-vh mx p-6 bg-body flex flex-col  text-customOrange"
+          className="mb-2  100-vh mx p-6  flex flex-col  text-customOrange"
         >
           HELLO CURIO
         </motion.h1>
+      </div>
+      <div>
+      <motion.div
+      className="starOrange"
+      style={{
+        width: '200px', // Adjust the size as needed
+        height: '200px', // Adjust the size as needed
+        background: `url(${starOrange})`, // Set the SVG as background
+        backgroundSize: 'cover', // Ensure SVG covers the div
+        position: 'absolute', // Ensure the SVG remains fixed in the background
+        top: 20, // Position at the top
+        right: 100, // P
+        zIndex: -1, // Place behind other content
+      }}
+      animate={{ rotate: 360 }} // Add spinning motion
+      transition={{ duration: 2, repeat: Infinity, ease: 'linear' }} // Configure spinning motion
+    />
       </div>
       <div>
         <motion.div
